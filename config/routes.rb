8b0e6e3dcfc24c7948a393156b91ddfe2ef95fad
blacchollywood <alexander.eser@googlemail.com>
2016-06-11 10:36:04 +0200

    added pagination and fancy other things

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'pages/Data'
 
-  root 'pages#home'
+  root "pins#index"
   get 'about' => "pages#about" #creates about path
 
   # The priority is based upon order of creation: first created -> highest priority.
